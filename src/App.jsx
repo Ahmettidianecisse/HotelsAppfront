2434309641aadd4bcf85ce0eee1cecd6f0b7628b
unknown <cisseahmettidiane@gmail.com>
2026-01-30 09:35:54 +0000

    rajout de jsx sur mes imports dans l'app pour le deploiment sur vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import HotelsList from "./pages/HotelsList";
-import ProtectedRoute from "./Components/ProtectedRoute";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import HotelsList from "./pages/HotelsList.jsx";
+import ProtectedRoute from "./Components/ProtectedRoute.jsx";
 
 export default function App() {
   return (
